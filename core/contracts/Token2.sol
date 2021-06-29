4d5cf8bc8475540d976c6449a7cf259d7570cfc5
aliteAkash <akash@aliteprojects.in>
2021-06-29 11:02:11 +0530

    Essential Updates

--- core/contracts/Token2.sol
+++ core/contracts/Token2.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.5.16;
 
+import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract Token2 is ERC20 {
-    constructor() ERC20('Token 2', 'TK2') public {
-        _mint(msg.sender, 100 * 7 ** 18);
-    }
+contract Token2 is ERC20Detailed, ERC20 {
+    constructor() ERC20Detailed('Token 2', 'TK2', 18) public {}
 }
--- core/contracts/UniswapV2Factory.sol
+++ core/contracts/UniswapV2Factory.sol
@@ -1,5 +1,4 @@
-//SPDX-License-Identifier:MIT
-pragma solidity >= 0.5.16;
+pragma solidity =0.5.16;
 
 import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
